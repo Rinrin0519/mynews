9e29d813dd58703f92477a5e16f0a125874e056d
Rikako Tachibana <s-rikako.tachibana@rakuten.com>
2021-09-15 15:23:36 +0900

    さよならの挨拶を追加

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-  // 以下を追記
   public function add()
   {
       return view('admin.news.create');
