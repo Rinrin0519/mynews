767d38b8872751e01ed95c0ab9f9d4c433d65479
Rikako Tachibana <s-rikako.tachibana@rakuten.com>
2021-09-16 15:35:43 +0900

    第3章第4部2

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,10 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class NewsController extends Controller
+class ProfileController extends Controller
 {
-  public function add()
-  {
-      return view('admin.news.create');
-  }
+    public function add()
+    {
+    return view('admin.profile.create');
+    }
+
+public function create()
+    {
+    return redirect('admin/profile/create');
+    }
+
+public function edit()
+    {
+    return view('admin.profile.edit');
+    } 
+
+public function update()
+    {
+    return redirect('admin/profile/edit');
+    }
 }
