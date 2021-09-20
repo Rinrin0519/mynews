4f33ee696fd2ce9511ba1708ecd854fd790ec298
Rikako Tachibana <s-rikako.tachibana@rakuten.com>
2021-09-20 16:23:28 +0900

    第3章第12部

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -4,21 +4,26 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <!-- CSRF Token -->
          {{-- 後の章で説明します --}}
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
         {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
         <title>@yield('title')</title>
+
         <!-- Scripts -->
          {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
         <script src="{{ asset('js/app.js') }}" defer></script>
+
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
         <!-- Styles -->
-        {{-- 次の1行を削除 --}}
-        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
-        {{-- 次の1行を追記 --}}
+        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        {{-- この章の後半で作成するCSSを読み込みます --}}
         <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
@@ -32,13 +37,16 @@
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
+
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
+
                         </ul>
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
+                        {{-- 以下を追記 --}}
                         <!-- Authentication Links -->
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
@@ -63,11 +71,14 @@
                                 </div>
                             </li>
                             @endguest
+                            {{-- 以上までを追記 --}}
+                        </ul>
                         </ul>
                     </div>
                 </div>
             </nav>
             {{-- ここまでナビゲーションバー --}}
+
             <main class="py-4">
                 {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
                 @yield('content')
